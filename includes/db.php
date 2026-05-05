1584d9e7ae391d575680fdc7f8d4e29f58397e37
Sultanah Hesham Alahmed <444203969@student.ksu.edu.sa>
2026-05-05 12:17:53 +0300

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,14 +1,14 @@
 <?php
-$host = "127.0.0.1";
-$user = "root";
-$password = "";
-$database = "discover_saudi";
+$host = "sql213.infinityfree.com";
+$user = "if0_41834406";
+$password = "dmXMBRUeQlC";
+$database = "if0_41834406_discover_saudi";
 $port = 3306;
 
 $conn = mysqli_connect($host, $user, $password, $database, $port);
 
 if (!$conn) {
-    die("&#1601;&#1588;&#1604; &#1575;&#1604;&#1575;&#1578;&#1589;&#1575;&#1604; &#1576;&#1602;&#1575;&#1593;&#1583;&#1577; &#1575;&#1604;&#1576;&#1610;&#1575;&#1606;&#1575;&#1578;: " . mysqli_connect_error());
+    die("فشل الاتصال بقاعدة البيانات: " . mysqli_connect_error());
 }
 
 mysqli_set_charset($conn, "utf8mb4");
